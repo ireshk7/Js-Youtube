29bdf7f594dd12978003ca100a15d2ed29cbd6f6
ireshk7 <149696563+ireshk7@users.noreply.github.com>
2024-10-04 11:05:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict " //treat all Js code as newer version 
+
+//alert (3+3) we are using node js not browsers
+
+//code readability should be high 
+
+//number
+//bigint
+//string =>""
+//boolean
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+//object 
+
+console.log(typeof undefined);//undefined 
+console.log(typeof null);//object 
+
