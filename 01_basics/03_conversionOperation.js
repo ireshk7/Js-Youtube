cc18f9f87ea39e646148b0d0ca43f43c55801301
ireshk7 <149696563+ireshk7@users.noreply.github.com>
2024-10-04 13:12:16 +0000

    Converion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score="33aba";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber=Number(score);
+// console.log(valueInNumber);
+
+// "33"=>33
+// "33abc"=>NaN
+//true =>1;true false=>0
+
+let isLoggedIn="Iresh";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 =>true; 0=>false
+// ""=>false
+//"hitest"=>true
+
+let someNumber = 33;
+
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
