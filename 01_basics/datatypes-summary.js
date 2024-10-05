d50145af37c03d21be4f0f5c0ebfada08718b683
ireshk7 <149696563+ireshk7@users.noreply.github.com>
2024-10-05 08:35:44 +0000

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+const id = Symbol('123');
+
+const anotherId =Symbol('123');
+console.log(id===anotherId);
+
+const heroes =["SpiderMan","IronMan","Captain America"];
+
+const myObj={
+    name:"Peter Parker",
+    age:17,
+}
+
+let myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof myFunction);
+
