26ce6e9498d711b240239f64a3d4d91f0c647295
ireshk7 <149696563+ireshk7@users.noreply.github.com>
2024-10-05 07:01:28 +0000

    Comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,25 +1,45 @@
-let score="33aba";
+// let score="33aba";
 
-// console.log(typeof score);
-// console.log(typeof (score));
-// let valueInNumber=Number(score);
-// console.log(valueInNumber);
+// // console.log(typeof score);
+// // console.log(typeof (score));
+// // let valueInNumber=Number(score);
+// // console.log(valueInNumber);
 
-// "33"=>33
-// "33abc"=>NaN
-//true =>1;true false=>0
+// // "33"=>33
+// // "33abc"=>NaN
+// //true =>1;true false=>0
 
-let isLoggedIn="Iresh";
+// let isLoggedIn="Iresh";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-// 1 =>true; 0=>false
-// ""=>false
-//"hitest"=>true
+// // 1 =>true; 0=>false
+// // ""=>false
+// //"hitest"=>true
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let stringNumber=String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// let stringNumber=String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+
+
+//******************************Operations **************************************** //
+
+let value = 3;
+let negValue = -value
+console.log(negValue);
+
+console.log(2**3);
+
+let str1="Hello"
+
+let str2= " Iresh "
+
+let str3 = str1+str2;
+console.log(str3);
+
+
+console.log("1"+2);
